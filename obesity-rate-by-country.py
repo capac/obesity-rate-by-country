210df39617affe5c912a5984d8131f108358d2d1
capac <capac@users.noreply.github.com>
2024-07-26 11:06:59 +0100

    Line plot code for 1990-2022 obesity rates

--- obesity-rate-by-country.py
+++ obesity-rate-by-country.py
@@ -4,53 +4,52 @@ from pathlib import Path
 
 plt.style.use('lineplot-style.mplstyle')
 
-# obesity rate working directory
-python_work_dir = Path.home() / 'Programming/Python/'
-work_dir = python_work_dir / 'data-visualization/obesity-rate-by-country/'
+# path to save plot
+work_dir = Path.home() / 'Programming/Python/data-visualization/obesity-rate-by-country'
 
 # obesity rate data
 data_dir = Path.home() / 'Programming/data/obesity-rate-by-country/'
 data_file = data_dir / 'BEFA58B_ALL_LATEST.csv'
 
 # life expectancy dataframe
-df = pd.read_csv(data_file)
+columns=['GEO_NAME_SHORT', 'DIM_TIME', 'DIM_SEX', 'RATE_PER_100_N', 'RATE_PER_100_NL', 'RATE_PER_100_NU']
+ob_rt_df = pd.read_csv(data_file, usecols=columns)
 
-print(df.head())
+ob_rt_df['GEO_NAME_SHORT'] = ob_rt_df['GEO_NAME_SHORT'].replace(to_replace={
+    'United Kingdom of Great Britain and Northern Ireland': 'United Kingdom',
+    'Republic of Korea': 'South Korea',
+    'United States of America': 'United States',
+    'Netherlands (Kingdom of the)': 'Netherlands'})
 
-# # life expectancy pivot table dataframe
-# le_yr_df = le_df.pivot(index='year', columns='location_name', values='val')
-# le_yr_df.rename(columns={'United States of America': 'United States',
-#                          'Republic of Korea': 'South Korea'}, inplace=True)
-#
-# # there are too many countries to plot, I've select just a few to show
-# unselected_countries = ['Australia', 'Belgium', 'Canada',
-#                         'Luxembourg', 'Netherlands', 'New Zealand',
-#                         'Switzerland', 'Austria', 'Denmark',
-#                         'Portugal', 'Finland', 'Sweden', 'Spain',]
-# selected_countries = ['United Kingdom', 'Germany', 'Greece',
-#                       'Ireland', 'Italy', 'Japan', 'South Korea',
-#                       'Norway', 'Singapore', 'United States', 'France',]
-#
-# select_countries_df = le_yr_df[selected_countries]
-#
-# # life expectancy plot from 1990 to 2019
-# fig, ax = plt.subplots()
-# ax.plot(select_countries_df.index, select_countries_df,
-#         label=select_countries_df.columns)
-#
-# ax.set_xlabel('Years')
-# ax.set_ylabel('Life expectancy (years)')
-# ax.set_title('Life expectancy from 1990 to 2019')
-#
-# for label in selected_countries:
-#     ax.annotate(label, (2019.1, select_countries_df.loc[2019, label]))
-#
-# # Set source text
-# ax.text(x=0.08, y=-0.02,
-#         s='''Source: "2019 IHME Global Burden of Disease Study Results"''',
-#         transform=fig.transFigure,
-#         ha='left', fontsize=11, alpha=0.7)
-#
-# ax.legend(loc='center right', fontsize=11,
-#           bbox_to_anchor=(0.195, 0.85), fancybox=True)
-# plt.savefig(work_dir / 'plots/life_expectancy-1990-2019.png')
+ob_rt_df.rename(columns={'GEO_NAME_SHORT': 'Country', 'DIM_TIME': 'Year',
+                         'RATE_PER_100_N': 'Obesity rate'}, inplace=True)
+
+selected_countries = ['United Kingdom', 'Germany', 'Greece', 'Mexico',
+                      'Finland', 'Italy', 'Japan', 'South Korea',
+                      'Netherlands', 'Singapore', 'United States', 'France',]
+
+slct_ctr_ob_rt_df = ob_rt_df[ob_rt_df['Country'].isin(selected_countries)]
+slct_ctr_ob_rt_df = slct_ctr_ob_rt_df.pivot_table(index='Year', columns='Country',
+                                                  values='Obesity rate', aggfunc='mean')
+
+# obesity rate from 1990 to 2022
+fig, ax = plt.subplots()
+ax.plot(slct_ctr_ob_rt_df.index, slct_ctr_ob_rt_df,
+        label=slct_ctr_ob_rt_df.columns)
+
+ax.set_xlabel('Years')
+ax.set_ylabel(r'Obesity rate (BMI $\geq$ 30)')
+ax.set_title('Obesity rate from 1990 to 2022')
+
+for label in selected_countries:
+    ax.annotate(label, (2022.1, slct_ctr_ob_rt_df.loc[2022, label]))
+
+# Set source text
+ax.text(x=0.08, y=-0.02,
+        s='''Source: "World health statistics 2024: monitoring health for the SDGs, sustainable development goals"''',
+        transform=fig.transFigure,
+        ha='left', fontsize=11, alpha=0.7)
+
+ax.legend(loc='center right', fontsize=11,
+          bbox_to_anchor=(0.195, 0.85), fancybox=True)
+plt.savefig(work_dir / 'plots/obesity_rate_1990_2022.png')
